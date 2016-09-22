86b60491ccef716ec5ba08e372d908acf573d600
Russell Wallace <russell.wallace@gmail.com>
2016-09-22 17:45:30 +0100

    project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace check_line_endings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
